7c2f4b981e603deaa8ae521d9a0a4bea8beeb982
WagonLee <www.ice.fire@gmail.com>
2025-01-07 21:01:34 +0200

    lvl 1 working 2

--- powerUpLevel2.cpp
+++ powerUpLevel2.cpp
@@ -1,28 +1,25 @@
-#include "PowerUpLevel1.h"
+#include "PowerUpLevel2.h"
 #include "graphics.h"
 #include <iostream>
 
 // Constructor
-PowerUpLevel1::PowerUpLevel1(GameState* state, int x, int y)
-    : PowerUpBase(state, x, y, 1) {}
+PowerUpLevel2::PowerUpLevel2(GameState* state, int x, int y)
+    : PowerUpBase(state, x, y, 2) {}
 
 // Apply effect
-void PowerUpLevel1::applyEffect() {
-    std::cout << "Level 1 Power-Up effect applied! Weakening MovingEnemies for 6 seconds." << std::endl;
-
-    // Use the centralized weakness logic in PowerUpBase
-    startWeakEffect(6.0f); // Apply weak effect for 6 seconds
+void PowerUpLevel2::applyEffect() {
+    std::cout << "Level 2 Power-Up effect applied!" << std::endl;
 }
 
 // Draw
-void PowerUpLevel1::draw() {
+void PowerUpLevel2::draw() {
     graphics::Brush br;
     br.outline_opacity = 0.0f;
 
-    // Brown for Level 1
-    br.fill_color[0] = 0.65f; // Brown color
-    br.fill_color[1] = 0.16f;
-    br.fill_color[2] = 0.16f;
+    // Dark Green for Level 2
+    br.fill_color[0] = 0.0f;
+    br.fill_color[1] = 0.39f;
+    br.fill_color[2] = 0.0f;
 
     float cellSize = 50.0f;
     float xPos = gridX * cellSize + cellSize / 2.0f;
